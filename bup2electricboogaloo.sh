3c64ef9ac64542f83896006ff04a158642a9acbd
Leo Burbridge <leo.burbridge1@gmail.com>
2023-03-11 02:14:54 +0000

    jesus fucking christ i almost lost all my work because i dont fucking understand git

--- bup2electricboogaloo.sh
+++ bup2electricboogaloo.sh
@@ -5,103 +5,134 @@
 #bupper: https://github.com/tobru/bupper
 
 
-#Basic functions
-yell() { echo "$0: $*" >&2; }
-die() { yell "${@:2} ($1)"; exit $1; }
-try() { "$@" || die $? "cannot $*"; }
-printline() { printf -- '-%.0s' {1..$(tput cols)} ; printf '\n'; }
-
-#Setup vars
-#THE LACK OF A TRAILING SLASH ON ANY PATH IS IMPORTANT AND RELIED UPON BY THE SCRIPT
-indpath=/bup/index
-project=/testingproj #$project is the name of the project/stack being backed up. It's used for the index path in bup init.
-projroot=/home/leo/testingproj #$projroot is the root of the project folder - a seperate index will be created for each subdirectory in this path.
-testmode=0
-#TODO Implement checks for trailing slash. Search cheatsheet for substrings
-
 function main() {
 
-readarray -t names < <(find $projroot/ -mindepth 1 -maxdepth 1 -type d | sed "s,${projroot},,g")
-IFS=$'\n' names=($(sort <<<"${names[*]}")); unset IFS
-#This runs find on $projroot with a min/max directory depth of 1, searching for directories only. It then sed's it so remove $projroot
-#so we have just the names of the directories we want to backup. This is all then fed into readarray to make the array "$names".
-#We then sort them, because it looks nicer in the output.
-
-readarray -t namesnoslash < <(find $projroot/ -mindepth 1 -maxdepth 1 -type d | sed "s,${projroot}/,,g")
-IFS=$'\n' namesnoslash=($(sort <<<"${namesnoslash[*]}")); unset IFS
-#Run it again. First one leaves in the preceding "/" in each element, this one makes a version without it by adding a "/" to the end of the replacement pattern. It should sort the same, right?
-#We want a version without the slash so that the remote server side of each bup command can make ${project}-${namenoslash} rather than a subdirectory, as bup apparently doesn't autocreate subdirectories??
-#Could probably ssh into the remote and make the subdirectory ourselves but that sounds like a lot of effort. So..
-
-total=${#names[@]} #This counts how many elements the array $names has, and sets $total to that value.
-total2=${#namesnoslash[@]} #..Also gonna count how many are in the 2nd version of it, because I don't really trust it to always give an identical number of elements.
-#I can't see why it wouldn't, but I can't see much of anything to be fair. I'm not a programmer. I'm just a bloke with a keyboard.
-
-
-if [[ $total != $total2 ]]; then
-    printf '%s\n\n' "OH SHIT, TOTALS DONT MATCH"
-    die #That's how little I trust this shit.
-fi
-
-
 #Check if the index folder exists, if it doesn't, then attempt to make it. Will die on failure.
-if [ ! -d $indpath$project ]; then
+if [ ! -d "$indroot_project" ]; then
     createprojectfolder
 fi
 
 
-printf '%s\n\n' "Bupping!"
-
-count=0
-for el in ${!names[@]} #For each element in the names/namesnoslash arrays, do some shit.
+printf '%s\n\n' "Bupping!"; printline
+declare -i count && count=0
+for el in "${!name[@]}" #For each element in the name/namenoslash arrays, init it, index it, and save it.
 do
     count+=1
-    printline
-    printf '%s\n' "Backup "$count" of "$total": "$namenoslash""
+    printf '%s\n' "Backup $count of $total: ${namenoslash[$el]}"
     bup_init    #inloop
     bup_index   #inloop
-    #TODO Implement bup_save function
+    # TODO 6 Implement bup_save function (have copied bup_index, needs editing)
+    printline
 done
-#TODO Implement using element number $el in each iteration of the loop
+# TODO 10 Implement "-n $backupname". Probably set it to todays date? IT HAS TO GO BEFORE --check AFAIK - see line 4 of the first example on the bup index manpage
+# TODO 11 Dependent on 10 - set up automated culling of old backups. Easy option - see "bup-prune-older"
+
 } #HERE ENDS MAIN()
 
 
 
+# TODO 9 Setup variable definition function
+# Do not worry about vars defined in a function not applying globally - apparently they just fucken do? (probably because we're not using it in a command substitution?)
+
+#Script specific definition function
+function definevars () {
+    
+    #Define root vars
+    #THE LACK OF A TRAILING SLASH ON ANY OF THESE PATHS IS IMPORTANT AND RELIED UPON BY THE SCRIPT - see td 2
+    indroot=/bup/index #the base path to the bup index
+    project=/testingproj #$project is the name of the project/stack being backed up. It's used for the index path in bup init.
+    projroot=/home/leo/testingproj #$projroot is the root of the project folder - a seperate index will be created for each subdirectory in this path.
+    testmode=1 #If this is 0, it's a real run. If it's anything else, dry run. Check this is actually true before running, PLEASE DEAR GOD. sleep deprived fucker.
+    # TODO 2 Implement checks for trailing slash. Search cheatsheet for substrings - see https://unix.stackexchange.com/a/423552
+    # TODO 3 Could simplify by grabbing $project from $projroot? see https://unix.stackexchange.com/a/423552
+    
+    readarray -t name < <(find $projroot/ -mindepth 1 -maxdepth 1 -type d | sed "s,${projroot},,g")
+    IFS=$'\n' name=($(sort <<<"${name[*]}")); unset IFS
+    #This runs find on $projroot with a min/max directory depth of 1, searching for directories only. It then sed's it so remove $projroot
+    #so we have just the names of the directories we want to backup. This is all then fed into readarray to make the array "$name".
+    #Finally we sort them, because it looks nicer in the output if they're in alphabetical order.
+    #We also run it a second time to make a version of $name without a preceding slash for use in the remote path.
+    readarray -t namenoslash < <(find $projroot/ -mindepth 1 -maxdepth 1 -type d | sed "s,${projroot}/,,g")
+    IFS=$'\n' namenoslash=($(sort <<<"${namenoslash[*]}")); unset IFS
+
+    # TODO 5 replace use of sed in these two with bash parameter expansion - sub-string removal
+    # see https://unix.stackexchange.com/a/423552
+    
+    
+    #Define dependent vars
+    #build some better variables out of the shit we have
+    #theoretical $project define here - see td 3 
+    indroot_project="$indroot""$project"
+    total=${#name[@]} #This counts how many elements the array $names has, and sets $total to that value.
+    total2=${#namenoslash[@]} #..Also gonna count how many are in the 2nd version of it, because I don't really trust it to always give an identical number of elements.
+
+    #Just a little test that hopefully never ever triggers.
+    if [[ $total != "$total2" ]]; then
+        printf '%s\n\n' "OH SHIT, TOTALS DONT MATCH"
+        die #That's how little I trust this shit.
+    fi
+}
 
 
-#Script specific functions
+#Script specific action functions
 #Function to create the project folder in index
 function createprojectfolder () {
-    printf '%s\n' ""$project" index folder doesn't exist. Attempting to create."
-    if mkdir -m 777 $indpath$project; then
-        printf '%s\n' "Created "$project" index folder."
-        chown $indpath$project
+    printf '%s\n' "$project index folder doesn't exist. Attempting to create."
+    if mkdir -m 777 $indroot$project; then
+        printf '%s\n' "Created $project index folder."
+        chown $indroot$project
     else
-        printf '%s\n' "Couldn't create "$project" index folder."
+        printf '%s\n' "Couldn't create $project index folder."
         die
     fi
 }
 
 
-#Function to initialise the index for an element of the project
+
+#Function to bup init the path for an element of the project
 function bup_init () { #inloop
+    #Print the command
+    printf '%s\n' "bup -d ${indroot_project}${name[$el]} init -r BupServer:/bup${project}_${namenoslash[$el]}"
+
     if [[ $testmode == 0 ]]; then
-        printf '%s\n' "bup -d "$indpath""$project""$name" init -r BupServer:/bup"$project""$namenoslash""
-        bup -d $indpath$project$i init -r BupServer:/bup$project$i
-    else
-        printf '%s\n' "bup -d "$indpath""$project""$name" init -r BupServer:/bup"$project""$namenoslash""
+        #Run it for real
+        bup -d "$indroot_project""${name[$el]}" init -r BupServer:/bup "$project"_"${namenoslash[$el]}"
+
     fi
 }
 
 
-#Function to index the element of the project
+#Function to bup index the path for an element of the project
 function bup_index () { #inloop
+    #Print the command
+    printf '%s\n' "bup -d ${indroot_project}${name[$el]} index --check ${project}_${namenoslash[$el]}"
+    
     if [[ $testmode == 0 ]]; then
-        printf '%s\n' "bup -d "$indpath""$project""$name" index --check "$projroot""$namenoslash""
-        bup -d $indpath$project$i index --check $projroot$i
-    else
-        printf '%s\n' "bup -d "$indpath""$project""$name" index --check "$projroot""$namenoslash""
+        #Run it for real    
+        bup -d "$indroot_project""${name[$el]}" index --check "$projroot"_"${namenoslash[$el]}"
+
     fi
 }
 
-main; exit
+
+#Function to bup save the path for an element of the project
+function bup_save () { #inloop
+    #Print the command
+    printf '%s\n' "bup -d ${indroot_project}${name[$el]} index --check ${project}_${namenoslash[$el]}"
+    
+    if [[ $testmode == 0 ]]; then
+        #Run it for real       
+        bup -d "$indroot_project""${name[$el]}"  index --check "$projroot"_"${namenoslash[$el]}"
+
+    fi
+}
+
+#Basic functions
+yell() { echo "$0: $*" >&2; }
+die() { yell "${@:2} ($1)"; exit $1; }
+try() { "$@" || die $? "cannot $*"; }
+printline() { printf -- '%.s─' $(seq 1 "$(tput cols)") ; printf '\n'; } https://stackoverflow.com/a/64267019
+# TODO 1 replace printline in utils/printline and utils/funcs with the one here as it actually works lol
+
+#run the functions!
+definevars && main; exit
